559a38d04b99fc77d3b67046f26065127cbc28c4
Iván Sánchez <ramon.sanchez68@gmail.com>
2023-09-30 23:15:14 +0200

    refactor: removed autogenerated tests in lib.rs

--- chip8/src/lib.rs
+++ chip8/src/lib.rs
@@ -2,18 +2,3 @@ pub mod cpu;
 pub(crate) mod memory;
 pub(crate) mod gpu;
 pub(crate) mod instructions;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
